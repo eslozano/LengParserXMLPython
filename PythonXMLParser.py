5219519e0be775035dd51d08106dd3a101480f5e
eslozano <eslozano@espol.edu.ec>
2014-02-19 01:18:23 -0500

    Proyecto terminado y organizado

--- PythonXMLParser.py
+++ PythonXMLParser.py
@@ -3,6 +3,38 @@ class Capability:
 	def __init__(self, name=None, value=None):
 		self.name = name
 		self.value = value
+	def __str__(self):
+		return self.id
+
+	def __repr__(self):
+		return self.id
+
+class Group:
+	def __init__(self, id=None, capabilities=None):
+		self.id = id
+		if capabilities is None:
+			self.capabilities = []
+		else:
+			self.capabilities = capabilities
+	def __str__(self):
+		return self.id
+	def __repr__(self):
+		return self.id
+
+class Device:
+	def __init__(self, id=None, user_agent=None, actual_device_root=None, fall_back=None, groups=None):
+		self.id=id
+		self.user_agent=user_agent
+		self.actual_device_root=actual_device_root
+		self.fall_back=fall_back
+		if groups is None:
+			self.groups = []
+		else:
+			self.groups = groups
+	def __str__(self):
+		return self.id
+	def __repr__(self):
+		return self.id
 
 def leerCapDeString(str):
 	c=re.search("<capability\s", str)
@@ -17,33 +49,7 @@ def leerCapDeString(str):
 			m2=re.split("\"+",m.group(0))
 			cap.value=m2[1]
 		return cap
-
-
-class Group:
-	def __init__(self, id=None, capabilities=None):
-		self.id = id
-		if capabilities is None:
-			self.capabilities = []
-		else:
-			self.capabilities = capabilities
-
-class Device:
-	def __init__(self, id=None, user_agent=None, actual_device_root=None, fall_back=None, groups=None):
-		self.id=id
-		self.user_agent=user_agent
-		self.actual_device_root=actual_device_root
-		self.fall_back=fall_back
-		if groups is None:
-			self.groups = []
-		else:
-			self.groups = groups
-
-	def __str__(self):
-		return self.id
-
-	def __repr__(self):
-		return self.id
-
+		
 def leerDevDeString(str):
 	c=re.search("<device\s", str)
 	if c:
